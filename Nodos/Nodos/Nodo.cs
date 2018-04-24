8e55f3b4819c17456bab050ea9b1943b8df6af5e
macanepa <macanepa@miuandes.cl>
2018-04-24 16:29:24 -0300

    Commit 1

---
+++ Nodos/Nodos/Nodo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodos
+{
+    class Nodo<T>
+    {
+        T contenido;
+
+        public Nodo(T contenido)
+        {
+            this.contenido = contenido;
+        }
+    }
+}
